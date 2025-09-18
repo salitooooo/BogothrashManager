8321a528ce86a46a5b50d8612e32a9892fda6836
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-09-18 14:40:50 -0500

    añado el proyecto plano al punto que ibamos

---
+++ BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/BogoThrashManagerApplication.java
@@ -0,0 +1,14 @@
+package co.edu.poli.BogoThrashManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogoThrashManagerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Sapopinga");
+		SpringApplication.run(BogoThrashManagerApplication.class, args);
+	}
+
+}
