153a3ade019634eeb5c65672d0385bcc1a97be71
salitooooo <salodorado2004@gmail.com>
2025-09-29 11:18:33 -0500

    foreing key #1

--- BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/modelo/DetallePedido.java
+++ BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/modelo/DetallePedido.java
@@ -2,6 +2,16 @@ package co.edu.poli.BogoThrashManager.modelo;
 
 import java.util.*;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +19,8 @@ import lombok.NoArgsConstructor;
 /**
  * 
  */
+@Entity
+@Table(name="DetallePedido")
 @Data  // Generates: getters, setters, toString(), equals(), hashCode() for all fields
 @NoArgsConstructor  // Generates default (no-arg) constructor (required for JPA)
 @AllArgsConstructor 
@@ -19,16 +31,22 @@ public class DetallePedido {
     /**
      * 
      */
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@OneToMany(cascade= CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name= "destallePedidoId")//foreing key en detalle pedido
     private long idDetalle;
 
     /**
      * 
      */
-    private List<Producto> productos;
-
+	@Column(name = "productos")
+	private List<Producto> productos;
+   
     /**
      * 
      */
+	@Column(name = "precioTotal")
     private long precioTotal;
 
 }
