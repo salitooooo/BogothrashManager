dfda705bd796d2c368bcb78ddf10a955127cb93d
SysZ2211 <anothersamwithz@gmail.com>
2025-09-29 18:52:18 -0500

    Documentacion prt 1 PedidoController

--- BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/Controller/PedidoController.java
+++ BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/Controller/PedidoController.java
@@ -1,6 +1,10 @@
 package co.edu.poli.BogoThrashManager.Controller;
 import co.edu.poli.BogoThrashManager.modelo.Pedido;
 import co.edu.poli.BogoThrashManager.service.PedidoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
+@Api(value = "pedidos", tags = "pedido")
 @RestController
 @RequestMapping("/api/pedidos")
 public class PedidoController {
@@ -21,24 +26,30 @@ public class PedidoController {
 @Autowired
 private PedidoService pedidoService;
 
+@ApiOperation(value = "Crea un pedido", notes = "Crea un pedido con todos sus datos")
 @PostMapping
 public ResponseEntity create(@RequestBody Pedido pedido) {
 Pedido saved = pedidoService.createPedido(pedido);
 return ResponseEntity.ok(saved);
 }
 
+@ApiOperation(value = "Consigue todos los pedidos")
 @GetMapping
 public ResponseEntity<List> getAll() {
 return ResponseEntity.ok(pedidoService.getAllPedidos());
 }
 
+@ApiOperation(value = "Encuentra un pedido por id", notes = "A travez de una id, busca un pedido en la base de datos")
 @GetMapping("/{id}")
-public ResponseEntity<Pedido> getById(@PathVariable Long id) {
+public ResponseEntity<Pedido> getById(
+		@ApiParam(value = "Id del pedido", required = true)
+		@PathVariable Long id) {
     Optional<Pedido> pedido = pedidoService.getPedidoById(id);
     return pedido.map(ResponseEntity::ok)
                  .orElse(ResponseEntity.notFound().build());
 }
 
+@ApiOperation(value = "Modifica un pedido por id", notes = "Crea una copia del pedido con la id, permitiendo modificarlo mas tarde")
 @PutMapping("/{id}")
 public ResponseEntity<Pedido> update(@PathVariable Long id, @RequestBody Pedido updatedPedido) {
     Optional<Pedido> updated = pedidoService.updatePedido(id, updatedPedido);
@@ -46,6 +57,7 @@ public ResponseEntity<Pedido> update(@PathVariable Long id, @RequestBody Pedido 
                   .orElse(ResponseEntity.notFound().build());  // 404 if not found
 }
 
+@ApiOperation(value = "Elimina un pedido", notes = "Elimina un pedido de la base de datos utilizando su id")
 @DeleteMapping("/{id}")
 public ResponseEntity<Void> delete(@PathVariable Long id) {
     boolean deleted = pedidoService.deletePedido(id);
