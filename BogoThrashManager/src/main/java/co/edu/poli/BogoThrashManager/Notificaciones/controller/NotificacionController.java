6c01592fe8d4fed1dba54da36dd7cbc60af8a8b8
salitooooo <salodorado2004@gmail.com>
2025-09-30 12:08:13 -0500

    rest controller notificacones

---
+++ BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/Notificaciones/controller/NotificacionController.java
@@ -0,0 +1,49 @@
+package co.edu.poli.BogoThrashManager.Notificaciones.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.poli.BogoThrashManager.RegistroPedidos.modelo.Pedido;
+import io.swagger.v3.oas.annotations.Parameter;
+
+@RestController
+@RequestMapping("/api/notificaciones")
+
+public class NotificacionController {
+	
+	@PostMapping
+	public ResponseEntity <String> create (){
+	return ResponseEntity.ok("notificacion exitosa");
+	
+	}
+	 @GetMapping
+	 public ResponseEntity <String> getAll() {
+	 return ResponseEntity.ok("notificacion");
+	    }
+	 @GetMapping("/{id}")
+	 public ResponseEntity<String> getById(){
+	 return ResponseEntity.ok("notificacion");
+	                     
+	 }
+	 @PutMapping("/{id}")
+	 public ResponseEntity <String> update(){
+	return ResponseEntity.ok("notificacion actuañizada");
+		 
+	 }
+	
+	 @DeleteMapping("/{id}")
+	 public ResponseEntity <String> delete(){
+	return ResponseEntity.ok("notificacion eliminada");
+	 }
+	
+
+}
