7d7d003aef46f2dab2881ccddf12ccd8f0bd7c7e
SysZ2211 <anothersamwithz@gmail.com>
2025-09-30 11:11:38 -0500

    Documentacion prt 3

--- BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/Controller/PedidoController.java
+++ BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/Controller/PedidoController.java
@@ -4,6 +4,8 @@ import co.edu.poli.BogoThrashManager.service.PedidoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,7 +29,10 @@ public class PedidoController {
 private PedidoService pedidoService;
 
 @ApiOperation(value = "Crea un pedido", notes = "Crea un pedido con todos sus datos")
-
+@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "operacion exitosa"),
+	    @ApiResponse(code = 404, message = "no se pudo crear el pedido")
+})
 @PostMapping
 public ResponseEntity create(
 		@ApiParam(value = "Nuevo pedido del cliente", required = true)
@@ -37,14 +42,20 @@ return ResponseEntity.ok(saved);
 }
 
 @ApiOperation(value = "Consigue todos los pedidos")
-
+@ApiResponses(value = {
+	      @ApiResponse(code = 200, message = "operacion exitosa"),
+	      @ApiResponse(code = 404, message = "no se pudo encontrar los pedidos o no existen")
+	  })
 @GetMapping
 public ResponseEntity<List> getAll() {
 return ResponseEntity.ok(pedidoService.getAllPedidos());
 }
 
 @ApiOperation(value = "Encuentra un pedido por id", notes = "A travez de una id, busca un pedido en la base de datos")
-
+@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "operacion exitosa"),
+	    @ApiResponse(code = 404, message = "no se pudo encontrar el pedido o no existe")
+})
 @GetMapping("/{id}")
 public ResponseEntity<Pedido> getById(
 		@ApiParam(value = "Id del pedido por buscar", required = true)
@@ -55,7 +66,10 @@ public ResponseEntity<Pedido> getById(
 }
 
 @ApiOperation(value = "Modifica un pedido por id", notes = "Crea una copia del pedido con la id, permitiendo modificarlo mas tarde")
-
+@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "operacion exitosa"),
+	    @ApiResponse(code = 404, message = "no se pudo encontrar el pedido o no existe")
+})
 @PutMapping("/{id}")
 public ResponseEntity<Pedido> update(
 		@ApiParam(value = "Id del pedido a cambiar", required = true)
@@ -68,7 +82,10 @@ public ResponseEntity<Pedido> update(
 }
 
 @ApiOperation(value = "Elimina un pedido", notes = "Elimina un pedido de la base de datos utilizando su id")
-
+@ApiResponses(value = {
+		@ApiResponse(code = 200, message = "operacion exitosa"),
+	    @ApiResponse(code = 404, message = "no se pudo encontrar el pedido o no existe")
+})
 @DeleteMapping("/{id}")
 public ResponseEntity<Void> delete(
 		@ApiParam(value = "Id del pedido a eliminar")
