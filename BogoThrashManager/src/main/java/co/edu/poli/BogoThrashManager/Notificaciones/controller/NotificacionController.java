d240efcc8d5f66476b6011c76bcbd54a38f5d6f4
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-10-02 11:28:06 -0500

    eliminacion de pedido repetido, y añadido de clase service en notificaciones

--- BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/Notificaciones/controller/NotificacionController.java
+++ BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/Notificaciones/controller/NotificacionController.java
@@ -3,6 +3,7 @@ package co.edu.poli.BogoThrashManager.Notificaciones.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.edu.poli.BogoThrashManager.Notificaciones.service.NotificacionService;
 import co.edu.poli.BogoThrashManager.RegistroPedidos.modelo.Pedido;
 import io.swagger.v3.oas.annotations.Parameter;
 
@@ -19,6 +21,9 @@ import io.swagger.v3.oas.annotations.Parameter;
 @RequestMapping("/api/notificaciones")
 
 public class NotificacionController {
+	
+	@Autowired
+	private NotificacionService notificacionService;
 	
 	@PostMapping
 	public ResponseEntity <String> create (){
