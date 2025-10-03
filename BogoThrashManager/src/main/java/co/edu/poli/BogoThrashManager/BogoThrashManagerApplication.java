c1ad290c1631c119d4f42acc44cfa1afa0df8810
SysZ2211 <anothersamwithz@gmail.com>
2025-10-03 12:34:34 -0500

    Merge branch 'master' of https://github.com/salitooooo/BogothrashManager

--- BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/BogoThrashManagerApplication.java
+++ BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/BogoThrashManagerApplication.java
@@ -2,7 +2,7 @@ package co.edu.poli.BogoThrashManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//xd
 @SpringBootApplication
 public class BogoThrashManagerApplication {
 
