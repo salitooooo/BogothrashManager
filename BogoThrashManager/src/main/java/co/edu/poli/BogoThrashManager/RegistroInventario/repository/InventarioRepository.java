aa6e6bc5fdc462aba69f91ec467a583f05278c57
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-10-15 15:16:11 -0500

    querries repo

--- BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/RegistroInventario/repository/InventarioRepository.java
+++ BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/RegistroInventario/repository/InventarioRepository.java
@@ -1,12 +1,18 @@
 package co.edu.poli.BogoThrashManager.RegistroInventario.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import co.edu.poli.BogoThrashManager.RegistroInventario.modelo.Producto;
 
 
-
+@Repository
 public interface InventarioRepository extends JpaRepository<Producto, Long> {
+	@Query("SELECT p FROM Producto p WHERE p.nombre = :nombre AND p.tipo.nombre = :tipoNombre AND p.EsDulce = :EsDulce AND p.EsVegano = :EsVegano")
+	public Producto findByNombreAndTipoNombreAndEsDulceAndEsVegano();
 	
+	@Query("SELECT p FROM Producto p WHERE p.nombre = :nombre AND p.tipo.nombre = :tipoNombre AND p.EsCaliente = :EsCaliente AND p.TieneAlcohol = :TieneAlcohol")
+	public Producto findByNombreAndTipoNombreAndEsCalienteAndTieneAlcohol();
 
 }
