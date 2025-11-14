ce0ee744c40de3d5a29e1c1174cb86576bebb83c
Chigwi <145591851+Chigwi@users.noreply.github.com>
2025-11-14 09:45:29 -0500

    esqueleto catalogo service

--- BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/Catalogo/service/CatalogoService.java
+++ BogoThrashManager/src/main/java/co/edu/poli/BogoThrashManager/Catalogo/service/CatalogoService.java
@@ -1,11 +1,34 @@
 package co.edu.poli.BogoThrashManager.Catalogo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import co.edu.poli.BogoThrashManager.Catalogo.modelo.Articulo;
+import co.edu.poli.BogoThrashManager.Catalogo.repository.CatalogoRepository;
+import co.edu.poli.BogoThrashManager.RegistroPedidos.dto.PedidoInsertDto;
+import co.edu.poli.BogoThrashManager.RegistroPedidos.modelo.Pedido;
 import lombok.Data;
 
 
 public class CatalogoService {
+	@Autowired
+	private CatalogoRepository catalogorepository;
 	
+	public Articulo createArticulo(PedidoInsertDto dto) throws Exception{
+		return null;
+	}
+	
+	public List<Articulo> getAllArticulos() {
+		return null;
+	}
 
+	public boolean deleteArticulo(Long id) {
+		return true;
+	}
 	
+	public Articulo getArticuloByNombre(String nombre) {
+		return null;
+	}
 
 }
